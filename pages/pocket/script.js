f62c1132f727803cd4ed75b24a1840bcafb140ed
Chitwan Singh <119756817+chitwan27@users.noreply.github.com>
2024-06-25 00:08:43 +0530

    add different pages

---
+++ pages/pocket/script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const nInput = document.getElementById("nInput");
+    const frame = document.querySelector("div");
+    const arr = await fetchData();
+    nInput.placeholder = "1 - " + Math.floor((arr.length)/10);
+    nInput.addEventListener("change", () => {
+        frame.innerHTML = "";
+        const num = nInput.value;
+        supplyLinks(arr,num,frame);
+    });
+});
+
+function supplyLinks(list,number,container) {
+    if (number>0) {
+        for(let i = ((number-1)*10); i < (number*10) && i < list.length; i++) {
+            const link = list[i];
+            const linkElement = document.createElement('p');
+            linkElement.setAttribute("allowfullscreen","");
+            container.appendChild(linkElement);
+            linkElement.height = 300;
+            linkElement.width = 600;
+            linkElement.textContent = link;
+        }
+    }
+}
+
+async function fetchData() {
+    try {
+        const response = await fetch('/api/pocket');
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();        
+    } 
+    catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
