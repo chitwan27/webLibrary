80b5fee664ad83166b8cd9b1793bdcc7d7002744
Chitwan Singh <119756817+chitwan27@users.noreply.github.com>
2024-06-24 14:34:15 +0530

    refactor for efficiency

--- public/script.js
+++ public/script.js
@@ -1,45 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
     const nInput = document.getElementById("nInput");
-    nInput.placeholder = "1-56";
-
-    nInput.addEventListener("change", () => {
-        const num = document.getElementById("nInput").value;
-        const div = document.querySelector("div");
-        dv.innerHTML = "";
-        fetchData(num,div);
-    });
-    
+    const frame = document.querySelector("div");
+    pageReferesher(nInput, frame);
 });
 
-async function fetchData(num,div) {
-    try {
-        // Fetch the data from the server
-        const response = await fetch('/api/data');
-        // Check if the response is OK (status in the range 200-299)
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        // Parse the response as JSON
-        const data = await response.json();
-        // Fill the links into the div element
-        supplyLinks(data,num,div);
-    } 
-    catch (error) {
-        // Handle any errors that occurred during the fetch or parsing
-        console.error('Error fetching data:', error);
-    }
+async function pageReferesher(npt, cntnr) {
+    const arr = await fetchData();
+    npt.addEventListener("change", () => {
+        cntnr.innerHTML = "";
+        const num = npt.value;
+        supplyLinks(arr,num,cntnr);
+    });
 }
 
-function supplyLinks(list,num,div) {
-    if (num>0) {
-        for(let i = ((num-1)*10); i < (num*10) && i < list.length; i++) {
+function supplyLinks(list,number,container) {
+    if (number>0) {
+        for(let i = ((number-1)*10); i < (number*10) && i < list.length; i++) {
             const link = "https://www.youtube.com/embed/" + list[i];
             const linkElement = document.createElement('iframe');
             linkElement.setAttribute("allowfullscreen","");
-            div.appendChild(linkElement);
+            container.appendChild(linkElement);
             linkElement.height = 300;
             linkElement.width = 600;
             linkElement.src = link;
         }
+    }
+}
+
+async function fetchData() {
+    try {
+        const response = await fetch('/api/data');
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();        
+    } 
+    catch (error) {
+        console.error('Error fetching data:', error);
     }
 }
